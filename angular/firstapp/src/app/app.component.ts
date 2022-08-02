670176df661b42dd7343b3b4bdc3e848741e7ade
Joseph Botros <jwasily@hotmail.com>
2022-08-02 13:18:20 +0300

    testinh

--- angular/firstapp/src/app/app.component.ts
+++ angular/firstapp/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,6 +12,8 @@ export class AppComponent {
   teamMembers: string[] = [];
   newVariable : string = 'Hello from Typecript variable';
   errorMessage: string = '';
+  noOfTeams: number = 0;
+  teams: string[][] = [];
   constructor(){
     console.log('hello from component constructor');
     
@@ -34,5 +37,30 @@ export class AppComponent {
   }
   changeInput(){
     this.memberName = 'joseph';
+  }
+  onTeamsNumbersInput(noOfTeamsInput: string){
+    this.noOfTeams = Number(noOfTeamsInput);
+  }
+  generateTeams(){
+    this.teams = [];
+    const allMembers: string[] = [...this.teamMembers];
+    while(allMembers.length != 0){
+      for(let i=0; i< this.noOfTeams; i++){
+        const randomIndex =  Math.floor(Math.random() * this.noOfTeams);
+        //get random member from copied array 
+        const member = allMembers.splice(randomIndex,1)[0];
+        if(this.teams[i]){
+            this.teams[i].push(member); //second and more member in teams
+        }
+        else {
+          this.teams[i] = [member]; // first member in each team
+        }
+       
+      }
+    }
+    this.teamMembers = [];
+    this.noOfTeams = 0;
+    console.log(this.teams);
+    
   }
 }
