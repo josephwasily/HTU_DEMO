c605768e9df215d2d6ea4de1dcab3b83d6178b63
Joseph Botros <jwasily@hotmail.com>
2022-08-09 14:02:46 +0300

    add changes

--- templateforms/src/app/form/form.component.ts
+++ templateforms/src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CatForm, CatsService } from '../services/cats.service';
 
@@ -9,8 +9,17 @@ import { CatForm, CatsService } from '../services/cats.service';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  catForm : CatForm = 
+  { "name": "ahsjkdhakjhada",
+   "catsInfo": 
+   { "catsName": "mocha", "catsEmail": "adasd@asdasd", "selfie": "hourly", "Milk": true, "Biscuits": false, "Cucumber": false }, 
+   "catsBffName": "adasdasd", "bffGender": "male", "story": "asdasasd" }
 
- 
+  catFood: any [] = [
+    { name:  'Milk', checked: true},
+    { name:  'Biscuits', checked: false},
+    { name:  'Cucumber',checked: false}
+  ]
   title = 'templateforms';
   constructor(private catService: CatsService,
     private router: Router
@@ -19,12 +28,27 @@ export class FormComponent implements OnInit {
   }
   onSubmit(form: NgForm){
     console.log(form);
-    const cat = form.value as CatForm;
-    this.catService.add(cat);
-    form.reset();
-    this.router.navigate(['preview']);
+    console.log(this.catForm);
+    // const cat = form.value as CatForm;
+    // this.catService.add(cat);
+    // form.reset();
+    // this.router.navigate(['preview']);
   }
   ngOnInit(): void {
   }
+  changeCheckbox(){
+    const selectedFood = this.catFood.filter((value,index)=> {
+      return (value.checked == true);
+    });
+    console.log(selectedFood);
+  }
+  fillForm(event : Event, form: NgForm){
+    event.preventDefault();
+    //form.controls['name'].setValue('ahsjkdhakjhada');
+    this.catForm.name = 'hello from 2-way data binding';
+    //(form.controls['catsInfo'] as FormGroup).controls['catsName'].setValue('mocha');
 
+    // form.reset();
+    // this.router.navigate(['preview']);
+  }
 }
--- templateforms/src/app/services/cats.service.ts
+++ templateforms/src/app/services/cats.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 
 
 export interface CatForm {
-  bffGender: string,
-  catsBffName: string,
-  catsEmail: string,
-  catsName: string,
-  favoriteFood: boolean,
-  name: string,
-  selfie:string,
-  story: string
-  };
-  
+  name:        string;
+  catsInfo:    CatsInfo;
+  catsBffName: string;
+  bffGender:   string;
+  story:       string;
+}
+
+export interface CatsInfo {
+  catsName:  string;
+  catsEmail: string;
+  selfie:    string;
+  Milk:      boolean;
+  Biscuits:  boolean;
+  Cucumber:  boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
